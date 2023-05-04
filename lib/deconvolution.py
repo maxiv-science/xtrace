2866d911b6f767dd2be583827be026b77cb05eb1
Zdenek Matej <zdenek.matej@maxiv.lu.se>
2023-05-04 17:14:39 +0200

    lib-extensions for the direct inversion

--- lib/deconvolution.py
+++ lib/deconvolution.py
@@ -5,8 +5,10 @@ import cupy
 import matplotlib.pyplot as plt
 import cupyx.scipy.sparse as cusp
 import cupyx
+import cupyx.scipy.sparse.linalg as cslinalg
 import time
 from functools import reduce
+import scipy
 
 def landweber(img, psf, w, iters):
     pixel_arr = np.array(img).reshape(-1)
@@ -33,9 +35,116 @@ def regularized_richard_lucy(img, psf, smooth_coeff, iters):
 def richard_lucy(img, psf, iters):
     pixel_arr = np.array(img).reshape(-1)
     pixel_arr_itr = pixel_arr.copy()
+    ipsf = psf.copy()
+    ipsf = psf.T
     for _ in range(iters):
         c = psf@pixel_arr_itr
         c[c == 0] = 1
-        sm = psf.T@(pixel_arr/c)
+        sm = ipsf@(pixel_arr/c)
         pixel_arr_itr = pixel_arr_itr*sm
     return pixel_arr_itr.reshape(img.shape).get()
+
+def richard_lucy_inv_factorized(img, psf, iters):
+    pixel_arr = np.array(img).reshape(-1)
+    pixel_arr_itr = pixel_arr.copy()
+    psf_factorized = factorized(psf)
+    ipsf = psf_factorized( cupy.eye(psf.shape[0], dtype=psf.dtype) ).T
+    for _ in range(iters):
+        c = psf@pixel_arr_itr
+        c[c == 0] = 1
+        sm = ipsf@(pixel_arr/c)
+        pixel_arr_itr = pixel_arr_itr*sm
+    return pixel_arr_itr.reshape(img.shape).get()
+
+def richard_lucy_inv_simple(img, psf, iters):
+    pixel_arr = np.array(img).reshape(-1)
+    pixel_arr_itr = pixel_arr.copy()
+    ipsf = cusp.diags(psf.diagonal())
+    ipsf.data = 1/ipsf.data
+    for _ in range(iters):
+        c = psf@pixel_arr_itr
+        c[c == 0] = 1
+        sm = ipsf@(pixel_arr/c)
+        pixel_arr_itr = pixel_arr_itr*sm
+    return pixel_arr_itr.reshape(img.shape).get()
+
+def factorized(G):
+    return cslinalg.factorized(G)
+
+def splu(G):
+    return scipy.sparse.linalg.splu(G.get(), permc_spec='MMD_AT_PLUS_A', diag_pivot_thresh=1e-4)
+
+def nneg_inv_factorized(img, fact_G, nneg=True):
+    inv_img = fact_G(cupy.array(img).flatten())
+    if (nneg):
+        inv_img[inv_img<0] = 0
+    return inv_img.get().reshape(img.shape)
+
+def nneg_inv_solve(img, inv_A, nneg=True):
+    inv_img = inv_A.solve(img.flatten())
+    if (nneg):
+        inv_img[inv_img<0] = 0
+    return inv_img.reshape(img.shape)
+
+def eye(m, **kwargs):
+    return cupyx.scipy.sparse.eye(m, **kwargs)
+
+def H4neigh(shape, dtype=np.float32):
+    m, n = shape[0], shape[1]
+    H = cusp.coo_matrix((m*n,m*n), dtype=dtype)
+    # dx (img)
+    nrows = m*(n-1)
+    idx = cupy.arange(nrows)
+    row, col, data = cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=dtype)
+    row[:nrows], col[:nrows], data[:nrows] = idx, idx%(n-1) + (idx//(n-1))*n, -1
+    row[nrows:], col[nrows:], data[nrows:] = idx, idx%(n-1) + (idx//(n-1))*n + 1, 1
+    B = cusp.coo_matrix((data, (row, col)), shape=(nrows,m*n))
+    H += B.transpose()@B
+    # dy (img)
+    nrows = (m-1)*n
+    idx = cupy.arange(nrows)
+    row, col, data = cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=dtype)
+    row[:nrows], col[:nrows], data[:nrows] = idx, idx, -1
+    row[nrows:], col[nrows:], data[nrows:] = idx, idx + n, 1
+    B = cusp.coo_matrix((data, (row, col)), shape=(nrows,m*n))
+    H += B.transpose()@B
+    return H
+
+def H8neigh(shape, dtype=np.float32):
+    m, n = shape[0], shape[1]
+    H = cusp.coo_matrix((m*n,m*n), dtype=dtype)
+    # dx (img)
+    nrows = m*(n-1)
+    idx = cupy.arange(nrows)
+    row, col, data = cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=dtype)
+    row[:nrows], col[:nrows], data[:nrows] = idx, idx%(n-1) + (idx//(n-1))*n, -1
+    row[nrows:], col[nrows:], data[nrows:] = idx, idx%(n-1) + (idx//(n-1))*n + 1, 1
+    B = cusp.coo_matrix((data, (row, col)), shape=(nrows,m*n))
+    H += B.transpose()@B
+    # dy (img)
+    nrows = (m-1)*n
+    idx = cupy.arange(nrows)
+    row, col, data = cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=dtype)
+    row[:nrows], col[:nrows], data[:nrows] = idx, idx, -1
+    row[nrows:], col[nrows:], data[nrows:] = idx, idx + n, 1
+    B = cusp.coo_matrix((data, (row, col)), shape=(nrows,m*n))
+    H += B.transpose()@B
+    # +dx+dy (img)
+    # ----------------------------------------------
+    nrows = (m-1)*(n-1)
+    idx = cupy.arange(nrows)
+    row, col, data = cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=dtype)
+    row[:nrows], col[:nrows], data[:nrows] = idx, idx + idx//(n-1), -1
+    row[nrows:], col[nrows:], data[nrows:] = idx, idx + (n+1) + idx//(n-1), 1
+    B = cusp.coo_matrix((data, (row, col)), shape=(nrows,m*n))
+    H += 0.5*B.transpose()@B
+    # -dx+dy (img)
+    # ----------------------------------------------
+    nrows = (m-1)*(n-1)
+    idx = cupy.arange(nrows)
+    row, col, data = cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=np.int32), cupy.zeros((2*nrows,), dtype=dtype)
+    row[:nrows], col[:nrows], data[:nrows] = idx, idx + 1 + idx//(n-1), -1
+    row[nrows:], col[nrows:], data[nrows:] = idx, idx + n + idx//(n-1), 1
+    B = cusp.coo_matrix((data, (row, col)), shape=(nrows,m*n))
+    H += 0.5*B.transpose()@B
+    return H
--- lib/utils.py
+++ lib/utils.py
@@ -1,9 +1,10 @@
 import os, json
 import numpy as np
+#import hdf5plugin
+import bitshuffle
 import h5py
 import matplotlib.pyplot as plt
 from scipy import ndimage
-import hdf5plugin
 from pyFAI.azimuthalIntegrator import AzimuthalIntegrator
 import math
 import itertools
@@ -13,6 +14,7 @@ import cupy as cp
 import xtrace
 import skimage.morphology as morph
 import cupyx.scipy.sparse as cusp
+import scipy.sparse
 import time
 from scipy.ndimage import zoom
 from scipy.signal import convolve2d
@@ -126,3 +128,114 @@ def azimutal_integrator(config):
         rot1=rot1, rot2=rot2, rot3=rot3,
         wavelength=config['wavelength']
     )
+
+def pad(a, pad_rows, pad_cols = None, pad_val = 0):
+    if pad_cols is None:
+        pad_cols = pad_rows
+
+    sh_ = a.shape
+    sh = (int(np.prod(sh_[:-2])),)+sh_[-2:] # flatten image stack shape
+
+    a_pad = np.full(sh[:1]+(sh[1]+2*pad_rows,sh[2]+2*pad_cols), pad_val, dtype=a.dtype)
+    a_pad[:,pad_rows:(pad_rows+sh[1]),pad_cols:(pad_cols+sh[2])] = a.reshape(sh)
+    a_pad = a_pad.reshape(sh_[:-2]+a_pad.shape[-2:])
+
+    return a_pad
+
+def pad_sparse(G, pad_rows, pad_cols = None, pad_val = None):
+    if pad_cols is None:
+        pad_cols = pad_rows
+
+    sh_ = G.shape
+    sh = (sh_[0]+2*pad_rows, sh_[1]+2*pad_cols)
+
+    rows = np.empty_like(G.indices)
+    scipy.sparse._sparsetools.expandptr(G.shape[0], G.indptr, rows)
+    cols = G.indices
+
+    if pad_val is None or pad_val==0:
+        rows += pad_rows
+        cols += pad_cols
+        G_pad = scipy.sparse.csr_matrix((G.data,(rows,cols)),shape=sh)
+    else:
+        rows += pad_rows
+        cols += pad_cols
+        _cols, _rows = np.meshgrid(np.arange(sh[1]),np.arange(pad_rows))
+        _data = np.full(_cols.shape, pad_val, dtype=G.dtype)
+        rows = np.concatenate((rows,_rows.flatten()))
+        cols = np.concatenate((cols,_cols.flatten()))
+        data = np.concatenate((G.data,_data.flatten()))
+        _cols, _rows = np.meshgrid(np.arange(sh[1]),np.arange(sh_[0]+pad_rows,sh_[0]+2*pad_rows))
+        rows = np.concatenate((rows,_rows.flatten()))
+        cols = np.concatenate((cols,_cols.flatten()))
+        data = np.concatenate((data,_data.flatten()))
+        _cols, _rows = np.meshgrid(np.arange(pad_cols),np.arange(pad_rows,sh_[0]+pad_rows))
+        _data = np.full(_cols.shape, pad_val, dtype=G.dtype)
+        rows = np.concatenate((rows,_rows.flatten()))
+        cols = np.concatenate((cols,_cols.flatten()))
+        data = np.concatenate((data,_data.flatten()))
+        _cols, _rows = np.meshgrid(np.arange(sh_[1]+pad_cols,sh_[1]+2*pad_cols),np.arange(pad_rows,sh_[0]+pad_rows))
+        rows = np.concatenate((rows,_rows.flatten()))
+        cols = np.concatenate((cols,_cols.flatten()))
+        data = np.concatenate((data,_data.flatten()))
+        G_pad = scipy.sparse.csr_matrix((data,(rows,cols)),shape=sh)
+
+    return G_pad
+
+
+def pad_psf(G, img_shape, pad_rows, pad_cols = None, pad_val = None):
+    if pad_cols is None:
+        pad_cols = pad_rows
+
+    rows = np.empty_like(G.indices)
+    scipy.sparse._sparsetools.expandptr(G.shape[0], G.indptr, rows)
+    cols = G.indices
+
+    # convert to image indices
+    rows_i, rows_j = np.unravel_index(rows, img_shape)
+    cols_i, cols_j = np.unravel_index(cols, img_shape)
+    # padding
+    rows_i += pad_rows
+    rows_j += pad_cols
+    cols_i += pad_rows
+    cols_j += pad_cols
+    # convert to flatten indices
+    rows = rows_i * (img_shape[1]+2*pad_cols) + rows_j
+    cols = cols_i * (img_shape[1]+2*pad_cols) + cols_j
+
+    # new shape
+    sh = (img_shape[0]+2*pad_rows)*(img_shape[1]+2*pad_cols)
+    sh = (sh,sh)
+
+    if pad_val is None or pad_val==0:
+        return scipy.sparse.csr_matrix((G.data,(rows,cols)),shape=sh)
+    else:
+        _cols, _rows = np.meshgrid(np.arange(img_shape[1]+2*pad_cols),np.arange(pad_rows))
+        _data = np.full(_cols.shape, pad_val, dtype=G.dtype)
+        rowsp = _rows.ravel()
+        colsp = _cols.ravel()
+        datap = _data.ravel()
+        _cols, _rows = np.meshgrid(np.arange(img_shape[1]+2*pad_cols),np.arange(img_shape[0]+pad_rows,img_shape[0]+2*pad_rows))
+        rowsp = np.concatenate((rowsp,_rows.ravel()))
+        colsp = np.concatenate((colsp,_cols.ravel()))
+        datap = np.concatenate((datap,_data.ravel()))
+        _cols, _rows = np.meshgrid(np.arange(pad_cols),np.arange(pad_rows,img_shape[0]+pad_rows))
+        _data = np.full(_cols.shape, pad_val, dtype=G.dtype)
+        rowsp = np.concatenate((rowsp,_rows.ravel()))
+        colsp = np.concatenate((colsp,_cols.ravel()))
+        datap = np.concatenate((datap,_data.ravel()))
+        _cols, _rows = np.meshgrid(np.arange(img_shape[1]+pad_cols,img_shape[1]+2*pad_cols),np.arange(pad_rows,img_shape[0]+pad_rows))
+        rowsp = np.concatenate((rowsp,_rows.ravel()))
+        colsp = np.concatenate((colsp,_cols.ravel()))
+        datap = np.concatenate((datap,_data.ravel()))
+        idx = rowsp * (img_shape[1]+2*pad_cols) + colsp
+        rows = np.concatenate((rows,idx.ravel()))
+        cols = np.concatenate((cols,idx.ravel()))
+        data = np.concatenate((G.data,datap.ravel()))
+        return scipy.sparse.csr_matrix((data,(rows,cols)),shape=sh)
+
+def csr_to_cupy(G):
+    rows = np.empty_like(G.indices)
+    scipy.sparse._sparsetools.expandptr(G.shape[0], G.indptr, rows)
+    cols = G.indices
+    return cusp.csr_matrix((cp.array(G.data),(cp.array(rows),cp.array(cols))),shape=G.shape)
